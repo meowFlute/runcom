34e10858d02a78701f2b326a11a4eefb799b751d
Scott Christensen <mscottchristensen@gmail.com>
2023-05-21 00:14:15 -0400

    updated colors of my output to be a color

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Colors to differentiate my notes from program execution
+PURPLE='\033[1;34m'
+NO_COLOR='\033[0m'
+# use the following for a quick copy-paste reference
+# echo -e "${PURPLE}${NO_COLOR}"
+
 # Here is a function to process the files and check for symlinks and such
 process_file()
 {
@@ -12,16 +18,16 @@ process_file()
 	if [ -h $old_file ]
 	then
 	    # ~/.old_file is a symlink
-	    echo "$old_file is an existing symlink, note the location of the old file:"
+	    echo -e "${PURPLE}$old_file is an existing symlink, note the location of the old file:${NO_COLOR}"
 	    ls -l $old_file
 	fi
-	echo "removing $old_file"
+	echo -e "${PURPLE}removing $old_file${NO_COLOR}"
 	rm $old_file
     else
-	echo "$old_file never existed, no worries"
+	echo -e "${PURPLE}$old_file never existed, no worries${NO_COLOR}"
     fi
-    
-    echo "creating symlink $old_file -> $new_file"
+
+    echo -e "${PURPLE}creating symlink $old_file -> $new_file${NO_COLOR}"
     ln -s $new_file $old_file
 }
 
@@ -47,6 +53,6 @@ process_file $home_vimrc $new_vimrc
 # the vimrc file needs the dracula stuff to be installed
 dracula_vim_destination=$HOME/.vim/pack/themes/start
 echo
-echo "cloning in dracula vim repo to $dracula_vim_destination/dracula"
+echo -e "${PURPLE}cloning in dracula vim repo to $dracula_vim_destination/dracula${NO_COLOR}"
 mkdir -p $dracula_vim_destination
 git clone https://github.com/dracula/vim.git $dracula_vim_destination/dracula
